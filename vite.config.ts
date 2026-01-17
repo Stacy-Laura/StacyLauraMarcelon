262c6c52ea15ed84670609c5f8c7b2ba44da448d
stacy-laura <stacy@pouhiou.fr>
2026-01-17 13:59:39 +0100

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://github.com/Stacy-Laura/StacyLauraMarcelon.git',
+})
